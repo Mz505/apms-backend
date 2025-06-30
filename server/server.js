6bd4b099c006ae39cd8805b4447ffd45aa9eab26
Mashal Zerak <140585955+Mz505@users.noreply.github.com>
2025-06-30 12:05:19 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ 
     message: 'Something went wrong!', 
-    error: err.stack // SHOW the error for now
+    error: err.stack // You SHOULD see the error now
   });
 });
 
